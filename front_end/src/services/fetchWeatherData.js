1eb40421cde752a2a1dcb23e2d36ba019b5dd1a3
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2024-04-03 13:20:07 +0530

    weather station and front end changes added

---
+++ front_end/src/services/fetchWeatherData.js
@@ -0,0 +1,26 @@
+const fetchWeatherData = async () => {
+  const endpoint = 'http://localhost:3001/api/weather';
+  const apiKey = '7867632hjjhghj23hghkjh2j3hk2h3kjh2kj3h'; 
+
+  try {
+      const response = await fetch(endpoint, {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'x-api-key': apiKey 
+          }
+      });
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      console.error("Failed to fetch weather data:", error);
+      return [];
+  }
+};
+
+export default fetchWeatherData;
